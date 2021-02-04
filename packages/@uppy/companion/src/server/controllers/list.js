00abd4c4ada18988305cfaf3c4cffe442c2a9617
stephentuso <tusodev@gmail.com>
2021-02-04 03:35:11 -0500

    fix: retry list with delay

--- packages/@uppy/companion/src/server/controllers/list.js
+++ packages/@uppy/companion/src/server/controllers/list.js
@@ -1,17 +1,34 @@
 const { errorToResponse } = require('../provider/error')
+const { retryWithDelay } = require('../helpers/utils')
 
 function list ({ query, params, companion }, res, next) {
   const token = companion.providerToken
 
-  companion.provider.list({ companion, token, directory: params.id, query }, (err, data) => {
-    if (err) {
-      const errResp = errorToResponse(err)
-      if (errResp) {
-        return res.status(errResp.code).json({ message: errResp.message })
+  const getPerformList = (isLast) => () => {
+    return new Promise((resolve, reject) => companion.provider.list({ companion, token, directory: params.id, query }, (err, data) => {
+      if (err) {
+        if (isLast) {
+          const errResp = errorToResponse(err)
+          if (errResp) {
+            return res.status(errResp.code).json({ message: errResp.message })
+          }
+          next(err)
+          resolve()
+          return
+        }
+        reject(err)
+        return
       }
-      return next(err)
-    }
-    return res.json(data)
+
+      res.json(data)
+      resolve()
+    }))
+  }
+
+  retryWithDelay({
+    retryDelays: [5000, 10000, 15000, 20000],
+    action: getPerformList(false),
+    lastAction: getPerformList(true)
   })
 }
 
